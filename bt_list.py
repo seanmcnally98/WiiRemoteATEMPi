b9d3c568da33df982779cc6e4d5c4c9f7f1894d7
seanmcnally98 <5464457+seanmcnally98@users.noreply.github.com>
2023-07-15 17:11:54 -0400

    Add files via upload

---
+++ bt_list.py
@@ -0,0 +1,36 @@
+import subprocess
+import re
+import time
+import sys
+
+def get_paired_devices():
+    output = subprocess.check_output(['bluetoothctl', 'paired-devices'])
+    lines = output.decode('utf-8').split('\n')
+    devices = []
+    for line in lines:
+        match = re.search(r'Device\s([0-9A-F:]{17})', line)
+        if match:
+            devices.append(match.group(1))
+    return devices
+
+def is_device_connected(device):
+    output = subprocess.check_output(['bluetoothctl', 'info', device])
+    output = output.decode('utf-8')
+    return 'Connected: yes' in output
+
+# Continually re-check the list of connected devices
+while True:
+    # Get paired devices
+    paired_devices = get_paired_devices()
+
+    # Check if target device is connected
+    if 'XX:XX:XX:XX:XX:XX' in paired_devices and is_device_connected('XX:XX:XX:XX:XX:XX'):
+        # We did it!
+        print("Found The Wii Remote!")
+        process = subprocess.Popen(['/bin/bash', '/home/pi/startchataigne.sh'])
+        sys.exit(0)
+        break
+
+    # Wait for a few seconds before re-checking
+    time.sleep(3)
+
